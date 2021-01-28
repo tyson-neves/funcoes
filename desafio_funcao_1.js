26b0fc7a32c87865afda28cd40f4cf4bd058833d
Pedro Neves <66841214+tyson-neves@users.noreply.github.com>
2021-01-27 22:55:33 -0300

    Desafio function

---
+++ desafio_funcao_1.js
@@ -0,0 +1,24 @@
+// uma função quevai reserber 2 paramentos e vamos executar o console.log e vamos fazer uma soma e uma subtração
+
+const num1 = 3;
+const num2 = 4;
+
+
+const sub1 = 11;
+const sub2 = 7;
+
+
+function conta1(n1, n2) {
+    console.log(n1 + n2); 
+}
+
+function conta2(n1, n2) {
+    console.log(n1 - n2); 
+}
+
+conta1(num1, num2); 
+conta1(31, 98); 
+conta2(sub1, sub2); 
+conta2(19, 52); 
+
+// podemos passar o parâmetro tanto com variáveis ou com números e até strings
