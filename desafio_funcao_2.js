c29dc70bef0e6dc414cb8dd8ee4248d4451d4e6a
Pedro Neves <66841214+tyson-neves@users.noreply.github.com>
2021-01-30 20:28:07 -0300

    Update desafio_funcao_2.js

--- desafio_funcao_2.js
+++ desafio_funcao_2.js
@@ -30,3 +30,23 @@ function notaDoAluno(n1) {
 
 let conceito = notaDoAluno(11)
 console.log(conceito);
+
+
+function notaParaConceito (nota) {
+    switch (Math.ceil(nota)) {
+    case 10: return("A+");
+    case 9: return("A");
+    case 8: return("B+");
+    case 7: return("B");
+    case 6: return("C+");
+    case 5: return("C");
+    case 4: return("D+"); 
+    case 3: return("D"); 
+    case 2: return("E+");
+    case 1: return("E");
+    case 0: return("F");
+    default: return ("Conceito inválido");
+    }
+}
+let conceitoDoAluno = notaParaConceito(8.6);
+console.log(` O conceito do aluno é ${conceitoDoAluno}`);
