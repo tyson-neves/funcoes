f8952ce8f0f2412be2a61a3af4b04588d7198584
Pedro Neves <66841214+tyson-neves@users.noreply.github.com>
2021-01-31 13:07:12 -0300

    Update desafio_funcao_3.js

--- desafio_funcao_3.js
+++ desafio_funcao_3.js
@@ -1,6 +1,6 @@
-const nota1 = 8;
-const nota2 = 8;
-const nota3 = 2;
+const nota1 = 7.8;
+const nota2 = 2;
+const nota3 = 1;
 
 function escolhaDasNotas (n1, n2, n3) {
     if (n1 >= n2 && n2 > n3) {
@@ -9,13 +9,15 @@ function escolhaDasNotas (n1, n2, n3) {
         return n2 + n1;
     } else if (n3 >= n1 && n1 >= n2) {
         return n3 + n1;
-    } else {
-        return n3 + n2
+    } else if (n3 >= n2 && n1 <=n2) {
+        return n3 + n2;
+    } else if (n1 >= n3 && n3 >= n2) {
+        return n1 + n3;
     }
     
 }
 let somaDasNotas = escolhaDasNotas(nota1, nota2, nota3);
-//console.log(somaDasNotas);
+console.log(somaDasNotas);
 
 
 function mediaDasNotas (n1) {
@@ -23,7 +25,7 @@ function mediaDasNotas (n1) {
 }
 
 let mediaDoAluno = mediaDasNotas(somaDasNotas);
-//console.log(mediaDoAluno);
+console.log(mediaDoAluno);
 
 
 function aprovacao (media) {
