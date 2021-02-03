f0e683106a252cc3181149e65c1833e91a653c7f
Pedro Neves <66841214+tyson-neves@users.noreply.github.com>
2021-02-02 21:22:05 -0300

    Create funcao_como_paremetro1.js

---
+++ funcao_como_paremetro1.js
@@ -0,0 +1,20 @@
+function executar(funcao) {
+    if (typeof funcao ==="function") {
+        console.log(funcao());
+    }
+}
+
+function bomDia() {
+    return "bom dia ";
+}
+
+executar(3);
+executar(bomDia);
+
+// voce passa a função "bomDia" como um parâmetro para a function "executar" que vai verificar se o parâmetro é uma function... se for ela vai imprimir o console que no caso foi chamado a ser a function "bomDia"
+
+const x = bomDia;
+const y = bomDia();
+
+console.log(x); // nesse caso o valor de x vai ser a funtion bomDia
+console.log(y); // nesse caso vai ser o return da function bomDia. Imagine o "const y = bomDia();" esse () como "eu vou pegar o que tem dentro dessa function."
